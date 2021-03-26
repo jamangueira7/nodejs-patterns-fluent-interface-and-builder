fda2a8870077c7b405a682c61812cf0403dcfa93
João Mangueira <jamjs7@gmail.com>
2021-03-26 20:19:46 -0300

    add metodos

--- src/fluentSQL.js
+++ src/fluentSQL.js
@@ -1,5 +1,9 @@
 export default class FluentSQLBluider {
     #database = [];
+    #limit = 0;
+    #select = [];
+    #where = [];
+    #orderBy = [];
 
     constructor({ database }) {
         this.#database = database;
@@ -7,6 +11,30 @@ export default class FluentSQLBluider {
 
     static for(database) {
         return new FluentSQLBluider({ database });
+    }
+
+    limit(max) {
+        this.#limit = max;
+
+        return this;
+    }
+
+    select(props) {
+        this.#select = props;
+
+        return this;
+    }
+
+    where(query) {
+        this.#where = query;
+
+        return this;
+    }
+
+    orderBy(field) {
+        this.#orderBy = field;
+
+        return this;
     }
 
     bluid() {
